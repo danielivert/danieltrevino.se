16c7428c59384d1dc4757b145eb553b3b1cc8fbb
Daniel Treviño <danielivert@gmail.com>
2018-10-04 21:42:44 +0200

    Add Bulma

--- src/components/Specializing/index.js
+++ src/components/Specializing/index.js
@@ -2,6 +2,20 @@
 import * as React from "react"
 import styles from "./Specializing.module.scss"
 
-const Specializing = () => <div className={styles.container} />
+const Specializing = () => (
+  <div className={styles.container}>
+    <div className={styles.title}>
+      <h3>What I am specialized</h3>
+    </div>
+    <div className="columns is-multiline">
+      <div className={`column is-one-third ${styles.skill}`}>1</div>
+      <div className={`column is-one-third ${styles.skill}`}>2</div>
+      <div className={`column is-one-third ${styles.skill}`}>3</div>
+      <div className={`column is-one-third ${styles.skill}`}>4</div>
+      <div className={`column is-one-third ${styles.skill}`}>5</div>
+      <div className={`column is-one-third ${styles.skill}`}>6</div>
+    </div>
+  </div>
+)
 
 export default Specializing
